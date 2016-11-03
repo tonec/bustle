a321a515b5908f92e1628929950ec7a459cc791a
Tony Coop <tony@thewholeworldwindow.co.uk>
2016-11-03 19:23:47 +0000

    Added Geocoder service and implemented for testing in Search component.

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,8 +1,50 @@
 import React, { Component } from 'react';
 import { View, StyleSheet } from 'react-native';
+import Geocoder from '../services/Geocoder';
 import { Input } from './common';
 
 class Search extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            currentPosition: 'unknown'
+        };
+    }
+
+    componentDidMount() {
+
+        navigator.geolocation.getCurrentPosition(
+            (position) => {
+                const { longitude, latitude } = position.coords;
+
+                Geocoder.setApiKey('AIzaSyDb3TSwTYnXVxkQJOdEY04klpGK9w-xhvw');
+
+                Geocoder.getFromCoords({
+                    lat: latitude,
+                    lng: longitude
+                }).then(
+                    json => {
+                        console.log(json.results[0]);
+                        const location = json.results[0].formatted_address;
+                            this.setState({ currentPosition: location });
+                        },
+                    error => {
+                        alert(error);
+                        }
+                );
+            },
+            (error) => {
+                alert(JSON.stringify(error));
+            },
+            {
+                enableHighAccuracy: true,
+                timeout: 20000,
+                maximumAge: 1000
+            }
+        );
+    }
 
     render() {
         return (
@@ -10,7 +52,7 @@ class Search extends Component {
                 <Input
                     label={'Location:'}
                     placeholder={'Location'}
-                    value={''}
+                    value={this.state.currentPosition}
                 />
                 <Input
                     label={'Destination:'}
