044e8659782a51f3a0e28c403f918c610fd5cea6
Tony Coop <tony@thewholeworldwindow.co.uk>
2016-11-07 22:22:13 +0000

    Fixed formatting.

--- src/components/__specs__/SearchPage.spec.js
+++ src/components/__specs__/SearchPage.spec.js
@@ -25,9 +25,9 @@ describe('<SearchPage />', () => {
   })
 
   it.skip('should display of the current location as the from point', () => {
-      // const button = shallow(<Button>Click me</Button>);
-      //
-      // expect(button.containsMatchingElement(TouchableOpacity)).to.equal(true);
-      // expect(button.containsMatchingElement(Text)).to.equal(true);
+    // const button = shallow(<Button>Click me</Button>)
+    //
+    // expect(button.containsMatchingElement(TouchableOpacity)).to.equal(true)
+    // expect(button.containsMatchingElement(Text)).to.equal(true)
   })
 })
