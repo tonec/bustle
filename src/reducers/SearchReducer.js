03498448cab5f3f83356ac799fce28e5a8e86374
Tony Coop <tony@thewholeworldwindow.co.uk>
2016-11-08 00:47:42 +0000

    Completed the location list update reducer to add an array of location names.

--- src/reducers/SearchReducer.js
+++ src/reducers/SearchReducer.js
@@ -11,7 +11,7 @@ const INITIAL_STATE = {
   locationText: null,
   destinationText: null,
   locationListUpdating: false,
-  locationList: null,
+  locationList: [],
   destinationList: null
 }
 
@@ -19,16 +19,23 @@ export default (state = INITIAL_STATE, action) => {
   console.log(action)
 
   switch (action.type) {
+
     case CURRENT_LOCATION_CHANGED:
       return { ...state, currentLocation: action.payload }
+
     case LOCATION_TEXT_CHANGED:
       return { ...state, locationText: action.payload }
+
     case DESTINATION_TEXT_CHANGED:
       return { ...state, destinationText: action.payload }
+
     case LOCATION_LIST_UPDATING:
       return { ...state, locationListUpdating: true }
+
     case LOCATION_LIST_UPDATED:
-      return { ...state, locationListUpdating: false, locationList: action.payload }
+      const locations = action.payload.predictions.map(item => item.description)
+      return { ...state, locationListUpdating: false, locationList: locations }
+
     default:
       return state
   }
