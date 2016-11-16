17a0a93a340f0bdb1716cd907f2f7f20aed55a7f
Tony Coop <tony@thewholeworldwindow.co.uk>
2016-11-16 19:34:10 +0000

    Added tfl auth creds

--- src/services/Globals.js
+++ src/services/Globals.js
@@ -2,7 +2,9 @@ const globals = {
   geocodingApiUrl: 'https://maps.google.com/maps/api/geocode/json',
   geocodingApiKey: 'AIzaSyDb3TSwTYnXVxkQJOdEY04klpGK9w-xhvw',
   placesApiUrl: 'https://maps.googleapis.com/maps/api/place/autocomplete/json',
-  placesApiKey: 'AIzaSyC7DKnSHHJ_82puLQ6LsO4Njicrx7u6iqE'
+  placesApiKey: 'AIzaSyC7DKnSHHJ_82puLQ6LsO4Njicrx7u6iqE',
+  tflAppId: '5d4004db',
+  tflAppKey: '1d3bb30df746546e01ac6643988c7020'
 }
 
 export default globals
